8a5752881c0ace418f24d192a66416bbfeba5928
yashodip deore <yashodipdeore2006@gmail.com>
2026-04-11 16:00:58 +0530

    Added session management and token generation for user authentication

--- poster/server.js
+++ poster/server.js
@@ -1,9 +1,16 @@
 const { text } = require("node:stream/consumers");
 const Butter = require("../butter");
 const { request } = require("node:http");
+const { error } = require("node:console");
 
 
 //SECTION --------------- Users & Posts Data ----------------//
+
+//ANCHOR - Sessions data------//!SECTION
+//{userId: 1, token: 23243143}
+const SESSIONS = [];
+
+
 //ANCHOR --- Users Data ------//
 const USERS = [
   { id: 1, name: "Om Patil", username: "om123", password: "string" },
@@ -41,6 +48,10 @@ server.route("get", '/login', (req, res) => {
   res.sendFile('./public/index.html', 'text/html');
 });
 
+server.route("get", "/profile", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
 server.route("get", "/styles.css", (req, res) => {
   res.sendFile("./public/styles.css", "text/css");
 });
@@ -55,6 +66,7 @@ server.route("get", "/scripts.js", (req, res) => {
 //SECTION ----------------- JSON Routes ------------------------ //
 
 //ANCHOR ----- Login functionality json route ----//
+//--- Log a user and give them a token------//
 server.route('post', '/api/login', (req, res) => {
   let body = '';
 
@@ -64,8 +76,6 @@ server.route('post', '/api/login', (req, res) => {
 
   req.on('end', () => {
     body = JSON.parse(body);
-    console.log(body);
-
 
     const username = body.username;
     const password = body.password;
@@ -78,6 +88,14 @@ server.route('post', '/api/login', (req, res) => {
     //check the password if user exists
     if (user && user.password === password) {
       //At this point, we know that the client is who they say they are
+
+      //Generating 10 digit's token & Saving it to the  Sessions array
+      const token = Math.floor(Math.random() * 100000000000).toString();
+      SESSIONS.push({ userId: user.id, token: token });
+
+      //passing to token to every path using cookies
+      res.setHeader('Set-Cookie', `token=${token}; Path=/;`);
+
       res.status(200).json({ message: 'Logged in successfully !' });
     } else {
       res.status(401).json({ message: 'Invalid user name or password !' });
@@ -86,9 +104,24 @@ server.route('post', '/api/login', (req, res) => {
 });
 
 
-//ANCHOR - 
-
+//ANCHOR ------ User Route ---------//
 server.route("get", '/api/user', (req, res) => {
+  const cookies = req.headers.cookie || '';
+  const token = cookies.split('; ').find(c => c.startsWith('token='))?.split('=')[1];
+
+
+  const session = SESSIONS.find((session) => {
+    return session.token === token;
+  });
+  if (session) {
+    //Send the users profile information
+  } else {
+    res.status(401).json({ error: "Unauthorized " });
+  }
+
+
+
+  console.log("Token:", token);
 
 });
 
